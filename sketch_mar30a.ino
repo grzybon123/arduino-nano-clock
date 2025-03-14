af7f39a7c8f8a071e0f402bdb7fdf74a13724eea
szymon Jańczewski <szymon.grzybon@gmail.com>
2025-03-14 14:33:36 +0100

    program works, all i need to add is button interrupts

--- sketch_mar30a.ino
+++ sketch_mar30a.ino
@@ -5,12 +5,12 @@ int* LisOhour[12];
 int* LisOlis[6];
 
 int lengthsofnum[12] = {3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6};
-int lisOleng[6] = {4, 10, 6, 7, 3, 4};
+int lisOleng[6] = {4, 10, 6, 8, 3, 4};
 
 // LED array setup
 
 // Global arrays (initialized with values)
-    #define NUM_LEDS 100
+    #define NUM_LEDS 110
     CRGB leds[NUM_LEDS];
 int one[3] = {44, 45, 46};
 int two[3] = {35, 34, 33};
@@ -25,12 +25,12 @@ int ten[3] = {0, 1, 2};
 int eleven[6] = {27, 28, 29, 30, 31, 32};
 int twelve[6] = {16, 15, 14, 13, 12, 11};
 
-int Half[4] = {87, 86, 85, 84};
-int TwentyFive[10] = {87, 86, 85, 84, 83, 82, 81, 80, 79, 78};
+int Half[4] = {66, 67, 68, 69};
+int TwentyFive[10] = {87, 86, 85, 84, 83, 82, 77, 80, 79, 78};
 int twenty[6] = {87, 86, 85, 84, 83, 82};
-int quar[7] = {90, 91, 92, 93, 94, 95, 96};
-int Ten[3] = {82, 81, 80};
-int Funf[4] = {81, 80, 79, 78};
+int quar[8] = {90, 91, 92, 93, 94, 95, 96,88};
+int Ten[3] = {71, 72, 73};
+int Funf[4] = {77, 80, 79, 78};
 
 // FastLED setup function
 void setup() {
@@ -39,7 +39,29 @@ void setup() {
 
 
 }
+int itis()
+    {
+      leds[109] = CRGB::Blue;
+      leds[108] = CRGB::Blue;
+      leds[106] = CRGB::Blue;
+      leds[105] = CRGB::Blue;
+    }
 
+int oblock(){
+      leds[5] = CRGB::Orange;
+      leds[6] = CRGB::Orange;
+      leds[7] = CRGB::Orange;
+      leds[8] = CRGB::Orange;
+      leds[9] = CRGB::Orange;
+      leds[10] = CRGB::Orange;
+}
+
+int past(){
+            leds[65] = CRGB::Blue;
+            leds[64] = CRGB::Blue;
+            leds[63] = CRGB::Blue;
+            leds[62] = CRGB::Blue;
+}
 // Main loop function
 void loop() {
     // Initialize LisOlis and LisOhour arrays
@@ -63,41 +85,50 @@ void loop() {
     LisOhour[10] = eleven;
     LisOhour[11] = twelve;
     for (int godz = 0; godz < 12; ++godz) {
-        for (int i = 0; i < lengthsofnum[godz]; i++) {
-            leds[LisOhour[godz][i]] = CRGB::Red;
-        }
-        FastLED.show();
-        delay(100);
-        FastLED.clear();
-
-        for (int el = 5; el > 0; --el) {
-            for (int i = 0; i < lengthsofnum[godz]; i++) {
-                leds[LisOhour[godz][i]] = CRGB::Red;
+        for (int el = 5; el >= 0; --el) {
+           // od x.30 do x.
+            for (int i = 0; i < lengthsofnum[godz]; i++) 
+            { //godzina 
+                leds[LisOhour[godz][i]] = CRGB::White;   
             }
-            for (int x = 0; x < lisOleng[el]; ++x) {
-                leds[LisOlis[el][x]] = CRGB::Red;
+            for (int x = 0; x < lisOleng[el]; x++) 
+            {  //dodatek
+                leds[LisOlis[el][x]] = CRGB::Green;
             }
-            leds[66] = CRGB::Red;
-            leds[67] = CRGB::Red;
-            leds[68] = CRGB::Red;
-            leds[69] = CRGB::Red;
+            past();
+            itis();
             FastLED.show();
-            delay(100);
+            delay(3000);
             FastLED.clear();
         }
+
+            for (int i = 0; i < lengthsofnum[godz]; i++) 
+            {
+                leds[LisOhour[godz][i]] = CRGB::Yellow;
+            } // godzina x.00
+            itis();
+            oblock();
+            FastLED.show();
+            delay(3000);
+            FastLED.clear();
 
         for (int el = 1; el < 6; ++el) {
-            for (int i = 0; i < lengthsofnum[godz]; i++) {
-                leds[LisOhour[godz][i]] = CRGB::Red;
+            for (int i = 0; i < lengthsofnum[godz]; i++) 
+            { // od 1 bo pół godziny już było zrobionew
+                leds[LisOhour[godz][i]] = CRGB::White; //godzina
             }
-            for (int x = 0; x < lisOleng[el]; ++x) {
-                leds[LisOlis[el][x]] = CRGB::Red;
+            for (int x = 0; x < lisOleng[el]; ++x) 
+            {
+                leds[LisOlis[el][x]] = CRGB::Yellow; //dodatek
             }
-            leds[77] = CRGB::Red;
-            leds[78] = CRGB::Red;
+            itis();
+            leds[75] = CRGB::Blue;  //to
+            leds[76] = CRGB::Blue;
             FastLED.show();
-            delay(100);
+            delay(3000);
             FastLED.clear();
         }
+
+
     }
 }
